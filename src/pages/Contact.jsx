aa24f98db3452fb789f9a78cf0dc6a640f5f1267
Pranavkangale <kangalepranav@gmail.com>
2024-06-12 22:05:15 +0530

    Responsiveness updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -59,9 +59,7 @@ const Contact = () => {
             <Col lg="5" md="5">
               <div className="contact__info">
                 <h6 className="fw-bold">Contact Information</h6>
-                <p className="section__description mb-0">
-                  123 ZindaBazar, Sylhet, Bangladesh
-                </p>
+                <p className="section__description mb-0">Pune ,India</p>
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="fs-6 mb-0">Phone:</h6>
                   <p className="section__description mb-0">+88683896366</p>
